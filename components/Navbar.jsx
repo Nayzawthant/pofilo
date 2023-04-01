39e21185edaab77052a4c4a787104783a6469af6
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2023-04-01 09:42:09 +0630

    for sidebar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -112,11 +112,6 @@ const Navbar = () => {
                 <AiOutlineClose />
               </div>
             </div>
-            <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4'>
-                Let&#39;s build something legendary together
-              </p>
-            </div>
           </div>
           <div className='py-4 flex flex-col'>
             <ul className='uppercase'>
@@ -138,11 +133,6 @@ const Navbar = () => {
               <Link href='/#projects'>
                 <li onClick={() => setNav(false)} className='py-4 text-sm'>
                   Projects
-                </li>
-              </Link>
-              <Link href='/resume'>
-                <li onClick={() => setNav(false)} className='py-4 text-sm'>
-                  Resume
                 </li>
               </Link>
               <Link href='/#contact'>
